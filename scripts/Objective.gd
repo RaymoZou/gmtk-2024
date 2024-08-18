e26a70f06806f5f245964e519328af5f9d829c67
Kevin Zou <kevin.zp.zou@gmail.com>
2024-08-18 15:03:14 -0700

    Untangle requirements from objectives

--- scripts/Objective.gd
+++ scripts/Objective.gd
@@ -1,17 +1,33 @@
 """
-An Objective is specific to a room type. Each Objective can have one or more Requirements, each of which is specific to a constraint (eg. area).
+An Objective is specific to a room type and has a specific type that indicates what type of Objective it is (eg. AreaObjective)
+
+All Objectives should implement _init and check methods
 """
 
 class_name Objective
 
 var description : String
 var room_type : Tile.TILE_TYPE
-var requirements : Array[Requirement]
+var type: ObjectiveType
 var status : Status
+
+enum ObjectiveType { AREA }
 enum Status {INCOMPLETE, COMPLETE}
 
-func _init(_description, _room_type, _requirements, _status,):
-	description = _description
-	room_type = _room_type
-	requirements = _requirements
-	status = _status
+class AreaObjective extends Objective:
+	var area: int
+
+	func _init(_description, _room_type, _area) -> void:
+		description = _description
+		room_type = _room_type
+		type = ObjectiveType.AREA
+		status = Status.INCOMPLETE
+		area = _area
+
+	func check(rooms: Array[Room]) -> void:
+		var room_found = false
+		for room in rooms:
+			if room.area >= area:
+				room_found = true
+				break
+		status = Status.COMPLETE if room_found else Status.INCOMPLETE
--- scripts/objective_manager.gd
+++ scripts/objective_manager.gd
@@ -8,38 +8,26 @@ signal objectives_updated
 
 # only called once
 func initialize_objectives():
-	# Initialize requirements for level 1
+	# Initialize objectives for level 1
 	# Stone
-	var stone_requirements: Array[Requirement] = [
-		Requirement.AreaRequirement.new(5),
-	]
-	var stone_objective = Objective.new(
+	var stone_objective = Objective.AreaObjective.new(
 		"Create a Stone room with an area of 5",
 		Tile.TILE_TYPE.STONE,
-		stone_requirements,
-		Objective.Status.INCOMPLETE
+		5,
 		)
 	objectives.push_front(stone_objective)
 	# Grass
-	var grass_requirements: Array[Requirement] = [
-		Requirement.AreaRequirement.new(3),
-	]
-	var grass_objective = Objective.new(
+	var grass_objective = Objective.AreaObjective.new(
 		"Create a Grass room with an area of 3",
 		Tile.TILE_TYPE.GRASS,
-		grass_requirements,
-		Objective.Status.INCOMPLETE
+		3,
 		)
 	objectives.push_front(grass_objective)
 	# Water
-	var water_requirements: Array[Requirement] = [
-		Requirement.AreaRequirement.new(8),
-	]
-	var water_objective = Objective.new(
+	var water_objective = Objective.AreaObjective.new(
 		"Create a Water room with an area of 8",
 		Tile.TILE_TYPE.WATER,
-		water_requirements,
-		Objective.Status.INCOMPLETE
+		8,
 		)
 	objectives.push_front(water_objective)
 	objectives_updated.emit()
@@ -53,28 +41,6 @@ func handle_room_updated(rooms : Array[Room]):
 	for objective in objectives:
 		# Filter by rooms for this objective
 		var matching_rooms = rooms.filter(func(room: Room): return room.type == objective.room_type)
-
-		for requirement in objective.requirements:
-			match requirement.type:
-				Requirement.ReqType.AREA:
-					check_area(matching_rooms, requirement)
-				_: # Default
-					pass
-	
-		# Check if all requirements have been fulfilled
-		# Then update objective status as appropriate
-		if objective.requirements.all(func(requirement: Requirement): return requirement.complete):
-			objective.status = Objective.Status.COMPLETE
-		else:
-			objective.status = Objective.Status.INCOMPLETE
+		objective.check(matching_rooms)
 
 	objectives_updated.emit()
-
-
-func check_area(rooms: Array[Room], requirement: Requirement.AreaRequirement):
-	var room_found = false
-	for room in rooms:
-		if room.area >= requirement.area:
-			room_found = true
-			break
-	requirement.complete = room_found
