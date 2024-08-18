86c39c861bba5587a3b6cf060cc0a32464e18cc8
RaymoZou <raymond.1zou@gmail.com>
2024-08-17 17:14:57 -0700

    add WASD camera movement

--- player_controller.gd
+++ player_controller.gd
@@ -2,9 +2,24 @@ extends Node
 
 @onready var tilemap_layer : TileMapLayer = $"../PlacementLayer"
 @onready var camera : Camera2D = $"../PlayerCamera"
-const ZOOM_AMOUNT : float = 0.1
+const ZOOM_AMOUNT : float = 0.3 # camera zoom speed
+const PAN_SPEED : float = 150 # camera pan speed
 
-# convert mouse position to tile coordinates
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_down"):
+		camera.position.y += PAN_SPEED * delta
+		
+	if Input.is_action_pressed("ui_up"):
+		camera.position.y -= PAN_SPEED * delta
+		
+	if Input.is_action_pressed("ui_left"):
+		camera.position.x -= PAN_SPEED * delta
+		
+	if Input.is_action_pressed("ui_right"):
+		camera.position.x += PAN_SPEED * delta
+		
+	if Input.is_action_pressed("ui_select"):
+		camera.position = Vector2.ZERO
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
@@ -15,6 +30,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		var tile_coords : Vector2 = tilemap_layer.get_tile_position()
 		tilemap_layer.remove_block(tile_coords)
 		
+	# camera zoom
 	if event.is_action_pressed("scroll_down"):
 		camera.zoom -= Vector2(ZOOM_AMOUNT, ZOOM_AMOUNT)
 		
