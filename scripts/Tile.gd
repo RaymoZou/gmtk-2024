9ecf6be045507a7cd0c075637769d50e9daac38e
RaymoZou <raymond.1zou@gmail.com>
2024-08-20 00:00:58 -0700

    update grass tile sprite

--- scripts/Tile.gd
+++ scripts/Tile.gd
@@ -3,7 +3,7 @@ class_name Tile
 # These are vectors that point to the coordinates of the tile in the tileset atlas
 # update here if the location of the sprite on the spritesheet changes
 const STONE_TILE = Vector2(8, 5)
-const GRASS_TILE = Vector2(1, 2)
+const GRASS_TILE = Vector2(3, 3)
 const WATER_TILE = Vector2(9, 7)
 
 enum TILE_TYPE { STONE, GRASS, WATER }
