97fb46c3ff71a0bed7b62ff29ee1a03e4b9872e7
Kevin Zou <kevin.zp.zou@gmail.com>
2024-08-20 00:37:24 -0700

    Scale UI text elements back to 1 so they look sharp

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,8 +1,5 @@
 extends CanvasLayer
 
-@onready var stone_button : Button = $ButtonContainerUI/Stone
-@onready var grass2_button : Button = $ButtonContainerUI/Grass2
-@onready var tile_label : RichTextLabel = $ButtonContainerUI/RichTextLabel
 @onready var tile_manager : TileMapLayer = $"../PlacementLayer"
 @onready var objective_manager = $"../ObjectiveManager"
 @onready var objective_label : RichTextLabel = $ObjectivesUI/RichTextLabel
@@ -25,15 +22,12 @@ func handle_objectives_updated():
 
 func handle_stone():
 	#print("changing to stone type")
-	tile_label.text = "tile: stone"
 	tile_manager.curr_tile = Tile.STONE_TILE
 	
 func handle_grass():
 	#print("grass type")
-	tile_label.text = "tile: grass"
 	tile_manager.curr_tile = Tile.GRASS_TILE
 
 func handle_water():
 	#print("water type")
-	tile_label.text = "tile: water"
 	tile_manager.curr_tile = Tile.WATER_TILE
