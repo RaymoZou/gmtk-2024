9d93d9e0a9eb9f96fc35df152b32bf0fa194f2a1
Kevin Zou <kevin.zp.zou@gmail.com>
2024-08-20 01:15:30 -0700

    Remove options menu item

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -4,7 +4,6 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus() # So that the player can use keyboard navigation
-	print('ready called')
 	get_tree().paused = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,11 +14,6 @@ func _process(delta: float) -> void:
 func _on_start_button_button_up() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
-
-
-func _on_options_button_button_up() -> void:
-	# TODO: Do we need an options button?
-	pass # Replace with function body.
 
 
 func _on_quit_button_button_up() -> void:
